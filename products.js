7101150962c91f86e8113bb203fd300d528d0ba3
mpousmali <mpousmali@csd.auth.gr>
2024-05-26 17:42:48 +0300

    changes

--- products.js
+++ products.js
@@ -8,7 +8,34 @@ window.onload = () => {
 
 searchButtonOnClick = () => {
     // BEGIN CODE HERE
-
+    const search=document.getElementById("inputSearch2000").value;
+    //console.log(search);
+    path="http://127.0.0.1:5000/search?"+"search="+search
+    fetch(path)
+    .then(response =>response.json())
+    .then(data =>
+     {    
+         let table = document.getElementById("dynamic_island").getElementsByTagName('tbody')[0];
+         table.innerHTML = '';
+         const x=data;
+         console.log(x.length);
+         for (let i = 0; i < data.length/6; i++){    
+         // Δημιουργία νέας γραμμής
+         let newRow = table.insertRow();
+         // Δημιουργία κελιών και προσθήκη δεδομένων
+         k=0;
+         for (let j = i*6; j < 6*(i+1); j++) {  
+          let newCell = newRow.insertCell(k);
+          Text="  "+data[j];
+          let newText = document.createTextNode(Text)
+          newCell.appendChild(newText);
+          k++;
+          }
+       }
+     } /////MHN ΞΕΧΑΣΩ ΤΑ ΚΕΦΑΛΑΙΑΑΑΑΑΑΑ
+     )
+    .catch(error => console.error(error));  
+    //array=search(name);
     // END CODE HERE
 }
 
