449f4ecb31cb8586e6aa792ae1020b26129edc47
mpousmali <mpousmali@csd.auth.gr>
2024-05-26 16:16:47 +0300

    commit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # BEGIN CODE HERE
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from pymongo import TEXT
+import base64
+import uuid
 
 
 import pymongo
@@ -17,15 +19,28 @@ client = pymongo.MongoClient(mongo_uri)
 db = client["pspi"]
 
 # Example: Inserting a document into a collection
-collection = db["products"]
-document = {"name": "example", "age": 25}
-collection.insert_one(document)
+#collection = db["products"]
+#document = {"id":"1","name": "f","production_year":2,"price":2,"color":1, "size":2}
+#collection.insert_one(document)
+
+#new_product = {}
+#new_product["id"] = "1"
+#new_product["name"] = "A4"
+#if (db.products.find({"name":new_product['name']}) is True):
+#    db.products.update_one ({"name":"paper"}, 
+#                           {'$set': {"production_year": 2023, "price":2, "color":2, "size":2}})
+#else:
+#    new_product["production_year"] = 2023
+#    new_product["price"] = 2
+#    new_product["color"] = 2
+#    new_product["size"] = 2
+#    collection.insert_one(new_product)
+
 
 # Example: Querying documents from the collection
-query_result = collection.find({"age": {"$gt": 20}})
-for doc in query_result:
-    print(doc)
-
+#query_result = collection.find({"age": {"$gt": 20}})
+#for doc in query_result:
+#    print(doc)
 
 
 
@@ -48,7 +63,64 @@ def search():
 @app.route("/add-product", methods=["POST"])
 def add_product():
     # BEGIN CODE HERE
-    return ""
+    #if (request.method=="POST"):
+    #information=request.form
+    #data = request.get_json()
+    #if (not data):
+    #    return jsonify('error')
+    #new_product = {}
+    #new_product["id"] = "1"
+    #new_product["name"] = request.args.get('name')
+    #if (db.products.find({"name":new_product['name']}) is True):
+    #    mongo.db.products.update_one ({"name":request.form['inputName']},
+    #                       {'$set': {"production_year": request.form['inputProductionYear'], "price":request.form['inputPrice'], "color":request.form['inputColor'], "size":request.form['inputSize']}})   
+    #else:
+    #    new_product["production_year"] = request.form['inputProductionYear']
+    #    new_product["price"] = request.form['inputPrice']
+    #    new_product["color"] = request.form['inputColor']
+    #    new_product["size"] = request.form['inputSize']
+    #    mongo.db.products.insert_one(new_product)
+    #print("hi")
+    
+    #return jsonify(new_product)
+
+    #return render_template("products.html",data=information)
+    str = request.args.get('name')
+    str_lower=str.lower()
+    #id=base64.urlsafe_b64encode(uuid.uuid1().bytes)
+    #str_id=id.replace('=', '')
+    #str_id=str_id[:5]
+    #id="a1"
+    index = str_lower.find('/')
+
+
+    inputName = str_lower[:index]
+    parts = str_lower.split('/')
+
+    # Initialize a dictionary to hold the key-value pairs
+    new_person = {}
+    #new_person['id']=str_id
+    new_person['name']=inputName
+    #new_person['id']=str(id)
+    
+    # Loop through the parts and extract key-value pairs
+    for i in parts[1:]:
+        key, value = i.split('=')
+        new_person[key] = value
+    exists = mongo.db.products.find_one({"name": new_person.get('name')})
+    if exists is None:
+        mongo.db.products.insert_one(new_person)
+    else:
+        mongo.db.products.update_one ({"name":new_person.get('name')},
+                           {'$set': {"production_year": new_person.get('production_year'),
+                                      "price":new_person.get('price'), 
+                                      "color":new_person.get('color'), 
+                                      "size":new_person.get('size')}})
+    return jsonify({"message":"success"})
+
+
+if (__name__=='__main__'):
+    app.run(debug=True,  host='127.0.0.1', port=5000)
     # END CODE HERE
 
 
--- products.js
+++ products.js
@@ -1,24 +1,5 @@
 const api = "http://127.0.0.1:5000";
 
-const KEY = 'put_here_your_API_key';
-let query = "";
-const numResults = 3;
-
-const input = document.querySelector('input');
-const form = document.querySelector('form');
-
-updateInput = (e) => {
-    query = e.target.value;
-}
-input.addEventListener('input', updateInput);
-
-submitted = (event) => {
-    event.preventDefault();
-    productFormOnSubmit();
-}
-
-form.onsubmit = submitted.bind(form);
-
 window.onload = () => {
     // BEGIN CODE HERE
 
@@ -33,6 +14,23 @@ searchButtonOnClick = () => {
 
 productFormOnSubmit = (event) => {
     // BEGIN CODE HERE
-
+    const inputName=document.getElementById("inputName").value;
+    const inputProductionYear=document.getElementById("inputProductionYear").value;
+    const inputPrice=document.getElementById("inputPrice").value;
+    const inputColor=document.getElementById("inputColor").value;
+    const inputSize=document.getElementById("inputSize").value;
+    //"http://127.0.0.1:5000/add-product?"+"inputName="+name+"inputProductionYear"+year+"inputPrice"+price+"inputColor"+color+"inputSize"+size
+    //console.log(name);
+    fetch("http://127.0.0.1:5000/add-product?"+"name="+inputName+"/production_year="+inputProductionYear+"/price="+inputPrice+"/color="+inputColor+"/size="+inputSize, {
+        method: 'POST',
+       body: JSON.stringify()
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        })
+    .catch((error) => {
+        console.error('Error:', error);
+        })
     // END CODE HERE
 }
