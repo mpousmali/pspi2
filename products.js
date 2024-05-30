a9a758a81422ee2274492d170641e5452a8b5de2
mpousmali <mpousmali@csd.auth.gr>
2024-05-30 20:20:54 +0300

    τρεχειιιιιιι

--- products.js
+++ products.js
@@ -64,7 +64,7 @@ productFormOnSubmit = (event) => {
             inputColor.toLowerCase()==="κοκκινο"||inputColor.toLowerCase()==="μπλε"||inputColor.toLowerCase()==="κιτρινο"||
             inputColor.toLowerCase()==="κόκκινο"||inputColor.toLowerCase()==="κίτρινο"||inputColor==="1"||inputColor==="2"||inputColor==="3")){
                 document.querySelector("."+"error").classList.add("display-error");
-                document.querySelector("."+"error").innerHTML="Invalid Color. 1: red, 2:yellow, 3:blue"
+                document.querySelector("."+"error").innerHTML="Invalid Color. 1: red, 2:yellow, 3:blue";
                 document.getElementById('form').style.display='none';
 
                 return false;
@@ -72,14 +72,14 @@ productFormOnSubmit = (event) => {
         if (!(inputSize.toLowerCase==="small"||inputSize.toLowerCase==="medium"||inputSize.toLowerCase==="large"||inputSize.toLowerCase==="extra large"||
             inputSize==="1"||inputSize==="2"||inputSize==="3"||inputSize==="4")){
                 document.querySelector("."+"error").classList.add("display-error");
-                document.querySelector("."+"error").innerHTML="Invalid Size. 1: small, 2: medium, 3:large, 4: extra large"
+                document.querySelector("."+"error").innerHTML="Invalid Size. 1: small, 2: medium, 3:large, 4: extra large";
                 document.getElementById('form').style.display='none';
 
                 return false;                
         }
         if (!(/^\d+$/.test(inputProductionYear))){
             document.querySelector("."+"error").classList.add("display-error");
-            document.querySelector("."+"error").innerHTML="Production year must be a positive integer"
+            document.querySelector("."+"error").innerHTML="Production year must be a positive integer";
             document.getElementById('form').style.display='none';
             event.preventDefault();
             return false;             
@@ -106,10 +106,7 @@ productFormOnSubmit = (event) => {
 
             return false;              
         }
-        document.querySelector("."+"success").classList.add("display-success");
-        document.querySelector("."+"success").innerHTML="Your request has been completed successfully";
-        document.getElementById('form').style.display='none';
-        event.preventDefault();
+
     //"http://127.0.0.1:5000/add-product?"+"inputName="+name+"inputProductionYear"+year+"inputPrice"+price+"inputColor"+color+"inputSize"+size
     //console.log(name);
     fetch("http://127.0.0.1:5000/add-product?"+"name="+inputName+"/production_year="+inputProductionYear+"/price="+inputPrice+"/color="+inputColor+"/size="+inputSize, {
@@ -118,6 +115,10 @@ productFormOnSubmit = (event) => {
     })
     .then(response => response.json())
     .then(data => {
+        document.querySelector("."+"success").classList.add("display-success");
+        document.querySelector("."+"success").innerHTML="Your request has been completed successfully";
+        document.getElementById('form').style.display='none';
+        event.preventDefault();
         })
     .catch((error) => {
         console.error('Error:', error);
